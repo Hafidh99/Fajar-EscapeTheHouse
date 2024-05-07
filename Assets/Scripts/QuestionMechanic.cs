e4f48537e811005fcab22a56d6293ded2f75b9b8
Nomsies <thopek007@gmail.com>
2024-05-07 12:52:53 +0700

    Fix Question Puzzle

--- Assets/Scripts/QuestionMechanic.cs
+++ Assets/Scripts/QuestionMechanic.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class QuestionMechanic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject[] rightAnswer;
+    [SerializeField] GameObject puzzleObj;
 
-    // Update is called once per frame
-    void Update()
+    public void checkAnswer()
     {
-        
+        int value = 0;
+
+        for (int i = 0; i < rightAnswer.Length; i++)
+        {
+            if (rightAnswer[i].transform.GetChild(1).gameObject.activeSelf) value++;
+            Debug.Log(value);
+        }
+
+        if (value >= 3)
+        {
+            Debug.Log("Puzzle berhasil diselesaikan!");
+
+            puzzleObj.transform.GetChild(0).gameObject.SetActive(false);
+            puzzleObj.GetComponent<ObjectInteract>().enabled = false;
+        }
+        else
+        {
+            Debug.Log("puzzle gagal diselesaikan");
+
+            puzzleObj.GetComponent<ObjectInteract>().enabled = true;
+        }
     }
 }
